981edc7c51f59b7a7881de161fef5807f99f4a69
Eirik Brandtzæg <eirikb@eirikb.no>
2012-04-01 14:49:08 +0200

    Supporting new version

---
+++ node.js/test/thoth.js
@@ -0,0 +1,93 @@
+var thoth = require('../thoth-cli.js'),
+should = require('should');
+
+describe('thoth', function() {
+    var id = '';
+
+    it('should create new thoths without error', function(done) {
+        thoth.create('Hello, world', function(err, parent) {
+            parent.should.have.property('id');
+            parent.should.have.property('versions', 1);
+            id = parent.id;
+            done();
+        });
+    });
+
+    it('should read created thoth parent without error', function(done) {
+        thoth.readJSON(id, function(err, parent) {
+            parent.should.have.property('id');
+            parent.should.have.property('versions', 1);
+            done();
+        });
+    });
+
+    it('should read created thoth version JSON without error', function(done) {
+        thoth.readJSON(id + '/1', function(err, item) {
+            item.data.should.equal('Hello, world');
+            item.should.have.property('timestamp');
+            done();
+        });
+    });
+
+    it('should read created thoth version without error', function(done) {
+        thoth.read(id + '/1', function(err, item) {
+            item.should.include('Hello, world');
+            done();
+        });
+    });
+
+    it('should read created thoth version attr without error', function(done) {
+        thoth.read(id + '/1.data', function(err, item) {
+            item.should.equal('Hello, world');
+            done();
+        });
+    });
+
+    it('should create new thoths version without error', function(done) {
+        thoth.create(id, 'Hello, world again', function(err, parent) {
+            parent.should.have.property('id', id);
+            parent.should.have.property('versions', 2);
+            done();
+        });
+    });
+
+    it('should read created thoth version attr without error', function(done) {
+        thoth.read(id + '/2.data', function(err, item) {
+            item.should.equal('Hello, world again');
+            done();
+        });
+    });
+
+    it('should read unkown id with error', function(done) {
+        thoth.read('asdf', function(err, item) {
+            should.exist(err);
+            should.not.exist(item);
+            done();
+        });
+    });
+
+    it('should read JSON unkown id with error', function(done) {
+        thoth.readJSON('asdf', function(err, item) {
+            should.exist(err);
+            should.not.exist(item);
+            done();
+        });
+    });
+
+    it('should read unkown version with error', function(done) {
+        thoth.read(id + '/asdf', function(err, item) {
+            should.exist(err);
+            should.not.exist(item);
+            done();
+        });
+    });
+
+    it('should create unkown id with with error', function(done) {
+        thoth.create('asdf', 'asdf', function(err, parent) {
+            should.exist(err);
+            should.not.exist(parent);
+            done();
+        });
+    });
+});
+
--- node.js/thoth-cli.js
+++ node.js/thoth-cli.js
@@ -8,26 +8,46 @@ exports.create = function(id, data, cb) {
         data = id;
         id = '';
     }
-    if (arguments.length === 3) id = '/' + id;
-
+    //if (arguments.length === 3) id = '/' + id;
     request.post({
-        url: host + 'create' + id,
+        url: host + id,
         json: {
             data: data
         }
     },
     function(err, res, data) {
-        if (cb) {
-            if (err) cb(err);
-            else cb(null, data);
+        if (err) cb(err);
+        else if (res.statusCode !== 200) cb(new Error(data));
+        else cb(null, data);
+    });
+};
+
+exports.readJSON = function(id, cb) {
+    request(host + id, function(err, res, data) {
+        if (err) {
+            cb(err);
+        } else if (res.statusCode !== 200) {
+            cb(new Error(data));
+        } else {
+            try {
+                data = JSON.parse(data);
+                cb(null, data);
+            } catch(e) {
+                cb(e);
+            }
         }
     });
 };
 
 exports.read = function(id, cb) {
-    request(host + 'read/' + id, function(err, res, data) {
-        if (err) cb(err);
-        else cb(null, data);
+    request(host + id, function(err, res, data) {
+        if (err) {
+            cb(err);
+        } else if (res.statusCode !== 200) {
+            cb(new Error(data));
+        } else {
+            cb(null, data);
+        }
     });
 };
 
